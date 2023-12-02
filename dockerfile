a2a5fb8bab2691965038c01c90fd2bcff4b827f8
Raul Calvo <raul.cal.bar@gmail.com>
2023-12-02 12:14:52 +0100

    updated script to load database

--- dockerfile
+++ dockerfile
@@ -6,13 +6,13 @@ ENV MYSQL_USER=cinema
 ENV MYSQL_PASSWORD=cinema
 
 ADD sql_scripts/AAScriptTFGCine_Grupo.sql /docker-entrypoint-initdb.d/
-ADD sql_scripts/cinemarl_cards.sql /docker-entrypoint-initdb.d/
-ADD sql_scripts/cinemarl_films.sql /docker-entrypoint-initdb.d/
-ADD sql_scripts/cinemarl_items_in_order.sql /docker-entrypoint-initdb.d/
-ADD sql_scripts/cinemarl_orders.sql /docker-entrypoint-initdb.d/
-ADD sql_scripts/cinemarl_product_type.sql /docker-entrypoint-initdb.d/
-ADD sql_scripts/cinemarl_projections.sql /docker-entrypoint-initdb.d/
-ADD sql_scripts/cinemarl_screens.sql /docker-entrypoint-initdb.d/
+# ADD sql_scripts/cinemarl_cards.sql /docker-entrypoint-initdb.d/
+# ADD sql_scripts/cinemarl_films.sql /docker-entrypoint-initdb.d/
+# ADD sql_scripts/cinemarl_items_in_order.sql /docker-entrypoint-initdb.d/
+# ADD sql_scripts/cinemarl_orders.sql /docker-entrypoint-initdb.d/
+# ADD sql_scripts/cinemarl_product_type.sql /docker-entrypoint-initdb.d/
+# ADD sql_scripts/zcinemarl_projections.sql /docker-entrypoint-initdb.d/
+# ADD sql_scripts/cinemarl_screens.sql /docker-entrypoint-initdb.d/
 # ADD sql_scripts/cinemarl_user_cards.sql /docker-entrypoint-initdb.d/
 # ADD sql_scripts/cinemarl_users.sql /docker-entrypoint-initdb.d/
 
--- sql_scripts/AAScriptTFGCine_Grupo.sql
+++ sql_scripts/AAScriptTFGCine_Grupo.sql
@@ -13,6 +13,8 @@ CREATE TABLE users (
   registerDate DATE
 );
 
+INSERT INTO `users` VALUES (1,'Yolanda','Calvo','2001-09-09','yoli@gmail.com','$2a$10$a40K6BRMcnVJmW.StO5Gwed4uz74M8TcJ3cHvpEAqFdvrXXOgT0iy',1,'2023-11-09'),(2, 'Juan', 'Gómez López', '1963-03-17', 'jgomez@gmail.com','$2a$10$aMS69IPQGlbZYjxvcJuZGuVDvCS/qJh5JDjBajWbTvHns8DfT7tMO',1,'2023-11-09');
+
 CREATE TABLE cards (
   idCard INT(11) AUTO_INCREMENT PRIMARY KEY,
   number BIGINT,
@@ -21,6 +23,9 @@ CREATE TABLE cards (
   cvv INT(3) NOT NULL
 );
 
+INSERT INTO `cards` VALUES (1,5540500001000004,'Yolanda Calvo','2025-12-31',456),(2,5540500001009999,'Sergio Ruiz','2025-12-31',4711);
+
+
 CREATE TABLE users_cards (
   idUser INT NOT NULL,
   idCard INT NOT NULL,
@@ -28,6 +33,9 @@ CREATE TABLE users_cards (
   foreign key(idCard) references cards(idCard),
   PRIMARY KEY(idUser, idCard)
 );
+
+INSERT INTO `users_cards` VALUES (1,1);
+
 
 CREATE TABLE orders (
   idOrder INT AUTO_INCREMENT PRIMARY KEY,
@@ -46,6 +54,8 @@ CREATE TABLE screens(
   specialSeats INT NOT NULL
 );
 
+INSERT INTO `screens` VALUES (1,'2D',50,8),(2,'3D',60,10);
+
 CREATE TABLE films(
   idFilm INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(50) NOT NULL,
@@ -63,7 +73,122 @@ CREATE TABLE films(
   awards VARCHAR(200),
   score DOUBLE,
   comingsoon boolean,
-  image VARCHAR(500)
+  images VARCHAR(500)
+);
+
+INSERT INTO `films` VALUES (
+  NULL,
+  'El Rey León',
+  1994,
+  NULL,
+  '1994-01-01',
+  '100min',
+  'Cartoon',
+  'Rob Minkoff, Roger Allers',
+  'Walt Disney',
+  'Simba, Mufasa, Nala, Scar, Zazú',
+  'Ambientada en la sabana africana donde ha nacido el futuro rey. Simba idolatra a su padre, el rey Mufasa, y se toma muy en serio su propio destino real. Pero en el reino no todos celebran la llegada del nuevo cachorro. Scar, el hermano de Mufasa y antiguo heredero al trono, tiene sus propios planes. La batalla por Pride Rock está asolada por la traición, la tragedia y el drama, y termina con el exilio de Simba. Con la ayuda de un curioso par de nuevos amigos, Simba tendrá que aprender a madurar y a recuperar lo que le pertenece por derecho.',
+  'Español',
+  'Estados Unidos',
+  '',
+  4.55,
+  0,
+  'http://image.tmdb.org/t/p/w500//sKCr78MXSLixwmZ8DyJLrpMsd15.jpg'
+);
+INSERT INTO `films` VALUES (
+  NULL,
+  'El Padrino',
+  1972,
+  'R',
+  '1972-03-14',
+  '175min',
+  'Crimen, Drama',
+  'Francis Ford Coppola',
+  'Mario Puzo, Francis Ford Coppola',
+  'Marlon Brando, Al Pacino, James Caan',
+  'La historia se centra en la poderosa familia de mafiosos italianos de Nueva York, liderada por Don Vito Corleone. Cuando se desata la violencia entre las familias rivales, Michael Corleone, el hijo más joven, se ve obligado a tomar las riendas del imperio criminal de su padre.',
+  'Español',
+  'Estados Unidos',
+  '3 Premios de la Academia',
+  4.8,
+  0,
+  'http://image.tmdb.org/t/p/w500//3bhkrj58Vtu7enYsRolD1fZdja1.jpg'
+);
+INSERT INTO `films` VALUES (
+  NULL,
+  'Titanic',
+  1997,
+  'PG-13',
+  '1997-11-18',
+  '195min',
+  'Drama, Romance',
+  'James Cameron',
+  'James Cameron',
+  'Leonardo DiCaprio, Kate Winslet, Billy Zane',
+  'La historia de amor épica entre Jack y Rose, dos jóvenes de diferentes clases sociales, a bordo del famoso y desafortunado transatlántico Titanic.',
+  'Español',
+  'Estados Unidos',
+  '11 Premios de la Academia',
+  4.7,
+  0,
+  'http://image.tmdb.org/t/p/w500//9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg'
+);
+INSERT INTO `films` VALUES (
+  NULL,
+  'El Laberinto del Fauno',
+  2006,
+  'R',
+  '2006-05-27',
+  '118min',
+  'Drama, Fantasía',
+  'Guillermo del Toro',
+  'Guillermo del Toro',
+  'Ivana Baquero, Sergi López, Maribel Verdú',
+  'En la España posguerra, una niña descubre un laberinto mágico que la lleva a un fauno. Con el tiempo, descubre su verdadera identidad y debe enfrentarse a desafíos sobrenaturales para cumplir con su destino.',
+  'Español',
+  'España, México, Estados Unidos',
+  '3 Premios de la Academia',
+  4.6,
+  0,
+  'http://image.tmdb.org/t/p/w500//7PurMm0gUOJZ8Uk0oyuZlt1CKfJ.jpg'
+);
+INSERT INTO `films` VALUES (
+  NULL,
+  'Y tu mamá también',
+  2001,
+  'R',
+  '2001-06-08',
+  '106min',
+  'Aventura, Comedia, Drama',
+  'Alfonso Cuarón',
+  'Carlos Cuarón, Alfonso Cuarón',
+  'Maribel Verdú, Gael García Bernal, Diego Luna',
+  'Dos adolescentes hacen un viaje por carretera con una mujer mayor. A medida que exploran la vida y la sexualidad, descubren verdades sobre ellos mismos y su relación con el mundo adulto.',
+  'Español',
+  'México',
+  'Premio del Festival de Cine de Venecia',
+  4.4,
+  0,
+  'http://image.tmdb.org/t/p/w500//izw41PkE8y891yraO6fN1tCNmOG.jpg'
+);
+INSERT INTO `films` VALUES (
+  NULL,
+  'El secreto de sus ojos',
+  2009,
+  'R',
+  '2009-08-13',
+  '129min',
+  'Crimen, Drama, Misterio',
+  'Juan José Campanella',
+  'Eduardo Sacheri, Juan José Campanella',
+  'Ricardo Darín, Soledad Villamil, Pablo Rago',
+  'Un oficial de la ley jubilado intenta resolver un caso de asesinato no resuelto mientras reflexiona sobre su vida pasada y su relación con una compañera de trabajo.',
+  'Español',
+  'Argentina, España',
+  'Premio de la Academia a la Mejor Película Extranjera',
+  4.5,
+  0,
+  'http://image.tmdb.org/t/p/w500//dkeAwfZzwL3WvToydE3CXiY80E0.jpg'
 );
 
 CREATE TABLE projections (
@@ -79,10 +204,15 @@ CREATE TABLE projections (
   foreign key(idScreen) references screens(idScreen)
 );
 
+INSERT INTO `projections` VALUES (5,'2023-11-30','2023-11-30',6.50,'[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]','[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]',1,2);
+
+
 CREATE TABLE product_type(
   idProductType INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL
 );
+
+INSERT INTO `product_type` VALUES (1,'Comida'),(2,'Merchandising');
 
 CREATE TABLE products (
   idProduct INT AUTO_INCREMENT PRIMARY KEY,
@@ -94,6 +224,9 @@ CREATE TABLE products (
   idProductType INT NOT NULL,
   foreign key(idProductType) references product_type(idProductType)
 );
+
+INSERT INTO `products` VALUES (1,'Palomitas',NULL,'Palomitas saladas',4.50,200,1),(2,'Toblerone',NULL,'Ñamyyy chocolate',2.50,0,1);
+
 
 CREATE TABLE items_in_order (
   idItemsOrder INT AUTO_INCREMENT PRIMARY KEY,
@@ -108,15 +241,3 @@ CREATE TABLE items_in_order (
   foreign key(idProduct) references products(idProduct),
   foreign key(idProjection) references projections(idProjection)
 );
-
--- CREATE USER cinema IDENTIFIED BY 'cinema';
--- grant all privileges on cinemaRL.* to cinema; 
--- FLUSH PRIVILEGES;
-
--- INSERTAR USUARIOS
--- select * from cinemaRL.users;
-INSERT INTO users (idUser, name, surname, birthdate, email, password) VALUES(2, 'Juan', 'Gómez López', '1963-03-17', 'jgomez@gmail.com','$2a$10$aMS69IPQGlbZYjxvcJuZGuVDvCS/qJh5JDjBajWbTvHns8DfT7tMO');
-
--- INSERTAR PRODUCTOS
-INSERT INTO product_type VALUES (1, 'Comida');
-INSERT INTO products VALUES (1, 'Palomitas', null, 'Riquísimas palomitas saladas', 4.5, 200, 1);
