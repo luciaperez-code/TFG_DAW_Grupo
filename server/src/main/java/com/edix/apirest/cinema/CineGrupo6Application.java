7d34f2b7bbbf2be4114ab3c05a2e6f85aa1fb377
Raul Calvo <raul.cal.bar@gmail.com>
2023-12-02 10:23:27 +0100

    added * origins to avoid cors

--- server/src/main/java/com/edix/apirest/cinema/CineGrupo6Application.java
+++ server/src/main/java/com/edix/apirest/cinema/CineGrupo6Application.java
@@ -2,6 +2,9 @@ package com.edix.apirest.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CineGrupo6Application {
@@ -9,5 +12,15 @@ public class CineGrupo6Application {
 	public static void main(String[] args) {
 		SpringApplication.run(CineGrupo6Application.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry reg) {
+                reg.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
 }
