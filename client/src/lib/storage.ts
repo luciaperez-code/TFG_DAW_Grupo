eb25ef4d1f7aea6fc27040289bacc2aae7ab496d
luciaperez-code <78657972+luciaperez-code@users.noreply.github.com>
2024-04-25 07:57:14 +0200

    Delete client directory

--- client/src/lib/storage.ts
+++
@@ -1,6 +0,0 @@
-export const storage = {
-    getAccessToken: () => JSON.parse(window.localStorage.getItem('access_token') || 'null'),
-    setAccessToken: (token: string) =>
-      window.localStorage.setItem('access_token', JSON.stringify(token)),
-    clearAccessToken: () => window.localStorage.removeItem('access_token'),
-  }
--- client/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-import FilmDetail from "./FilmDetail";
-import Films from "./Films";
-import NotFound from "./NotFound/NotFound";
-
-export { NotFound, Films, FilmDetail }
